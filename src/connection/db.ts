bbaaf9d62269e24b3ef0a62da068c5ad6f7305c1
Rohit-Vaddi <72375149+Rohit-Vaddi@users.noreply.github.com>
2023-01-28 15:38:03 +0530

    add live mongodb

--- src/connection/db.ts
+++ src/connection/db.ts
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const connection = () => {
-    mongoose.connect('mongodb://localhost/sastaStore').then(() => {
+    mongoose.connect('mongodb+srv://Gonna_Live:Gonna_Live_Pass@gonnalive.0hmfxfn.mongodb.net/Gonna_Live_DB').then(() => {
         console.log('DB connected successfully');
     }).catch((err) => {
         console.log('Error connecting to', err)
